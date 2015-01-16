43b85abfc315db86bd3bb786b6169bc160b9b1d9
Athan Clark <athan.clark@gmail.com>
2015-01-15 20:15:55 -0700

    Dat successful compile doe

--- src/Data/Graph/DAG.hs
+++ src/Data/Graph/DAG.hs
@@ -6,6 +6,9 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Data.Graph.DAG where
 
@@ -21,59 +24,66 @@ data EdgeValue (from :: Sym) (to :: Sym) = Edge
 -- | We need this for our list of edges.
 data EdgeKind = forall from to. EdgeType from to
 
--- | The basis for the exclusion map.
-type EmptyLoops = (  ('Foo, [])
-                  ': ('Bar, [])
-                  ': ('Baz, [])
-                  ': ('Qux, [])
-                  ': '[] )
-
 -- | Some people just want to watch the world burn.
-type family Deducible x :: Constraint
+type family Deducible (x :: Bool) :: Constraint
 type instance Deducible 'True = ()
 
 -- | @not . elem@ for lists of types, resulting in a constraint.
-type family Excluding (x :: k) (xs :: [k]) :: Constraint
-type instance Excluding a '[] = Deducible 'True       -- Basis
-type instance Excluding a (a ': ts) = Deducible 'False    -- Reject
-type instance Excluding a (b ': ts) = Excluding a ts
+type family Excluding (x :: k) (xs :: [k]) :: Constraint where
+  Excluding a '[] = Deducible 'True       -- Basis
+  Excluding a (a ': ts) = Deducible 'False    -- Reject
+  Excluding a (b ': ts) = Excluding a ts
 
 -- | A simple @Data.List.lookup@ function for type lists.
-type family Lookup
-              (index :: Sym)
-              ( map :: [(Sym, [Sym])] ) :: [Sym]
-type instance Lookup 'Foo ('Foo excludes ': xs) = excludes
-type instance Lookup a (b ': xs) = Lookup a xs
+type family Lookup (index :: Sym) ( map :: [(Sym, [Sym])] ) :: [Sym] where
+  Lookup a ( '( a, excludes ) ': xs ) = excludes
+  Lookup a (b ': xs) = Lookup a xs
 
 -- | Simply reject anything that's present in the list, and accept anything that
--- isnt & stuff. Non-deducability ~ refutation.
-type family Acceptable
-              (a :: EdgeKind)
-              ( oldLoops :: [(Sym, [Sym])] ) :: Constraint
-type instance (Excluding 'Foo (Lookup 'Bar excludeMap)) =>
-                Acceptable
-                  ('EdgeType 'Foo 'Bar)
-                  excludeMap = Deducible 'True
-type instance Acceptable
-                a
-                EmptyLoops = Deducible 'True
+-- isnt & stuff. Non-deducability / non-instance ~ refutation.
+class Acceptable (a :: EdgeKind) ( oldLoops :: [(Sym, [Sym])] ) where
+instance (Excluding 'Foo (Lookup 'Bar excludeMap)) =>
+            Acceptable ('EdgeType 'Foo 'Bar) excludeMap where
+instance Acceptable a (  '( 'Foo, '[] )
+          ': '( 'Bar, '[] )
+            ': '( 'Baz, '[] )
+              ': '( 'Qux, '[] )
+                ': '[] ) where
+
+type family AppendIfEqual (test :: Sym) (add :: Sym) (xs :: [Sym]) :: [Sym] where
+  AppendIfEqual t a (t ': xs) = a ': t ': xs
+  AppendIfEqual t a (u ': xs) = u ': (AppendIfEqual t a xs)
+  AppendIfEqual t a '[] = '[]
 
 -- | Update the exclusion map with the new edge
-type family (new :: EdgeKind)
-              :disallowedIn:
-                ( oldLoops :: [(Sym, [Sym])] ) :: [(Sym, [Sym])]
-type instance ('EdgeType from to)
-                :disallowedIn:
+type family DisallowedIn (new :: EdgeKind) ( oldLoops :: [(Sym, [Sym])] ) :: [(Sym, [Sym])] where
+-- | When `from ~ key`:
+  DisallowedIn ('EdgeType from to) ( '(from, xs) ': es) =
+    '(from, (to ': xs)) ': (DisallowedIn ('EdgeType from to) es) -- recurse down
+-- | When `from ~/~ key`, but `from ~ head value`
+-- FIXME: appending to an existing potential loop (note this is a list, not a
+--        unique list, so not 100% type safe :\)
+  DisallowedIn ('EdgeType from to) ( '(key, (from ': xs)) ': es ) =
+    ( '(key, (to ': from ': xs)) ': (DisallowedIn ('EdgeType from to) es) )
+-- | When `from ~/~ key`, and `from ~/~ head value`
+  DisallowedIn  ('EdgeType from to) ( '(key, (x ': xs)) ': es ) =
+    '(key, (x ': (AppendIfEqual from to xs))) ':
+        (DisallowedIn ('EdgeType from to) es)
+-- | Basis
+  DisallowedIn a '[] = '[]
                   -- TODO: Update `from` key, and add `to` to every key that
                   --       has `from` in it's value list.
 
 -- | @edges@ is a list of types with kind @EdgeKind@, while @nearLoops@ is a
 -- dictionary of the nodes transitively reachable by the index.
 data Dag (edges :: [EdgeKind]) ( nearLoops :: [(Sym, [Sym])] ) where
-  GNil :: Dag '[] EmptyLoops
+  GNil :: Dag '[] ('( 'Foo, '[] )
+                    ': '( 'Bar, '[] )
+                      ': '( 'Baz, '[] )
+                        ': '( 'Qux, '[] ) ': '[])
   GCons :: ( Acceptable b oldLoops
            , EdgeValue from to ~ a
            , EdgeType from to ~ b
            ) => a
              -> Dag old oldLoops
-             -> Dag (b ': old) (b :disallowedIn: oldLoops)
+             -> Dag (b ': old) (DisallowedIn b oldLoops)
