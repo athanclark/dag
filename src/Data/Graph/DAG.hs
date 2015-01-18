5bc234a835b5452732559e01d505e6beae5a08d7
Athan Clark <athan.clark@gmail.com>
2015-01-18 11:58:22 -0700

    Successful acyclicity. We still need uniqueness and better archtecture.

--- src/Data/Graph/DAG.hs
+++ src/Data/Graph/DAG.hs
@@ -37,49 +37,42 @@ type family Excluding (x :: k) (xs :: [k]) :: Constraint where
   Excluding a (b ': ts) = Excluding a ts
 
 -- | A simple @Data.List.lookup@ function for type lists.
-type family Lookup (index :: Sym) ( map :: [(Sym, [Sym])] ) :: [Sym] where
-  Lookup a ( '( a, excludes ) ': xs ) = excludes
+type family Lookup (index :: k) ( map :: [(k, k2)] ) :: k2 where
+  Lookup a ( '( a, v) ': xs ) = v
   Lookup a (b ': xs) = Lookup a xs
 
 -- | Simply reject anything that's present in the list, and accept anything that
 -- isnt & stuff. Non-deducability / non-instance ~ refutation.
+-- FIXME: No support for unique edges.
 class Acceptable (a :: EdgeKind) ( oldLoops :: [(Sym, [Sym])] ) where
 instance (Excluding from (Lookup to excludeMap)) =>
             Acceptable ('EdgeType from to) excludeMap where
 
-{-
-instance Acceptable a (  '( 'Foo, '[] )
-          ': '( 'Bar, '[] )
-            ': '( 'Baz, '[] )
-              ': '( 'Qux, '[] )
-                ': '[] ) where
--}
-
-type family AppendIfEqual (test :: Sym) (add :: Sym) (xs :: [Sym]) :: [Sym] where
-  AppendIfEqual t a (t ': xs) = a ': t ': xs
-  AppendIfEqual t a (u ': xs) = u ': (AppendIfEqual t a xs)
-  AppendIfEqual t a '[] = '[]
+-- | Add an explicit element to the head of a list, if the test is inside that
+-- list.
+type family PrependIfElem (test :: k) (a :: k) (xs :: [k]) :: [k] where
+  PrependIfElem t a (t ': xs) = a ': t ': xs
+  PrependIfElem t a (u ': xs) = u ': (PrependIfElem t a xs)
+  PrependIfElem t a '[]       = '[]
 
 -- | Update the exclusion map with the new edge: the @from@ key gets @to@ added,
--- and others with @from@ in it's value list will also get @to@.
-type family DisallowedIn (new :: EdgeKind) ( oldLoops :: [(Sym, [Sym])] ) :: [(Sym, [Sym])] where
+-- likewise with @from@ in it's value list.
+type family DisallowedIn
+              (new :: EdgeKind)
+              ( oldLoops :: [(Sym, [Sym])] ) :: [(Sym, [Sym])] where
 -- | When `from ~ key`:
   DisallowedIn ('EdgeType from to) ( '(from, xs) ': es) =
-    '(from, (to ': xs)) ': (DisallowedIn ('EdgeType from to) es) -- recurse down
--- | When `from ~/~ key`, but `from ~ head value`
--- FIXME: appending to an existing potential loop (note this is a list, not a
---        unique list, so not 100% type safe :\)
-  DisallowedIn ('EdgeType from to) ( '(key, (from ': xs)) ': es ) =
-    ( '(key, (to ': from ': xs)) ': (DisallowedIn ('EdgeType from to) es) )
+    '(from, (to ': xs)) ': -- add @to@ to transitive reach list
+      (DisallowedIn ('EdgeType from to) es) -- continue
 -- | When `from ~/~ key`, and `from ~/~ head value`
-  DisallowedIn  ('EdgeType from to) ( '(key, (x ': xs)) ': es ) =
-    '(key, (x ': (AppendIfEqual from to xs))) ':
-        (DisallowedIn ('EdgeType from to) es)
+  DisallowedIn  ('EdgeType from to) ( '(key, vs) ': es ) =
+    '(key, (PrependIfElem from to vs)) ': -- find the needle if it exists
+        (DisallowedIn ('EdgeType from to) es) -- continue
 -- | Basis
-  DisallowedIn a '[] = '[]
+  DisallowedIn a '[] = '[] -- search over.
 
 -- | @edges@ is a list of types with kind @EdgeKind@, while @nearLoops@ is a
--- dictionary of the nodes transitively reachable by the index.
+-- map of the nodes transitively reachable by each node.
 data Dag (edges :: [EdgeKind]) ( nearLoops :: [(Sym, [Sym])] ) where
   GNil :: Dag '[] ('( 'Foo, '[] )
                     ': '( 'Bar, '[] )
@@ -92,9 +85,3 @@ data Dag (edges :: [EdgeKind]) ( nearLoops :: [(Sym, [Sym])] ) where
            ) => !a
              -> Dag old oldLoops
              -> Dag (b ': old) c
-{-
-instance Show (Dag edges nearLoops) where
-  show GNil = "[]"
-  show (GCons (edge :: EdgeValue 'Foo 'Bar) xs) = "(foo -> bar) : " ++
-    show xs
--}
